9d4969387c6634d46a3d53910cbc0b1155e68f3e
unknown <cse12005038brur@gmail.com>
2026-08-26 12:51:54 +0600

    consultation and doctor details page done

---
+++ src/app/(commonLayout)/consultation/doctors/[id]/loading.tsx
@@ -0,0 +1,11 @@
+import HearbeatLoader from "@/components/shared/HearbeatLoader";
+
+const DoctorDetailsLoading = () => {
+  return (
+    <div>
+      <HearbeatLoader />
+    </div>
+  );
+};
+
+export default DoctorDetailsLoading;
